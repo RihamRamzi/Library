620f489462e1556d44ddcac3c2be367b5ab1bfb2
RihamRamzi <rihamsoftware@gmail.com>
2024-11-30 14:09:06 +0530

    in progress
    1. Display book on webpage function

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+addBookToLibrary("GOT", "IDK", 100, true);
+addBookToLibrary("LOL", "IDK", 100, false);
+
+const bookShelf = document.querySelector(".bookShelf");
+
+function displayBook() {
+  for (let book of myLibrary) {
+    // created book
+    const bookCover = document.createElement("div");
+    bookCover.classList.add("book");
+    // created Title
+    const title = document.createElement("span");
+    title.id = "T";
+    title.textContent = book.title;
+    // created Author
+    const author = document.createElement("span");
+    author.id = "A";
+    author.textContent = book.author;
+    // created pages
+    const pages = document.createElement("span");
+    pages.id = "P";
+    pages.textContent = `Pages: ${book.pages}`;
+    // created read
+    const read = document.createElement("span");
+    read.textContent = `Read: ${book.read}`;
+
+    bookCover.appendChild(title);
+    bookCover.appendChild(author);
+    bookCover.appendChild(pages);
+    bookCover.appendChild(read);
+    bookShelf.appendChild(bookCover);
+  }
+}
+
+displayBook();
